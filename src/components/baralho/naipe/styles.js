154fa92072825a1ade75c4b8e232757bc018d9e8
Marcelo Colla <colla.pr@gmail.com>
2019-07-20 12:32:23 -0300

    incluindo style responsável por exibir a naipe de uma carta

---
+++ src/components/baralho/naipe/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native'
+
+import { base, layout, theme } from '~/ui'
+
+export const BoxNaipe = styled.View`
+    ${base.flex}
+    ${base.center}
+
+    height: 80;
+`
+
+export const ImgNaipe = styled.Image`
+    width: 56;
+    height: 56;
+`
