423ebc447e386abe863e5c017b0526b08c38f18f
Marcelo Colla <colla.pr@gmail.com>
2019-07-21 14:06:02 -0300

    Incluindo actions de ranking

--- src/store/ducks/ranking.js
+++ src/store/ducks/ranking.js
@@ -1,6 +1,31 @@
-export default function ranking(state = {}, action = {}) {
+const initialState = []
+
+// RECORD fields:
+// {
+//     name: 'Amanda', countRound: 5, date: new Date(),
+// },
+
+export const Types = {
+    SAVE_RANKING: 'SAVE_RANKING',
+}
+
+export default function ranking(state = initialState, action = {}) {
     switch (action.type) {
+        case Types.SAVE_RANKING:
+            const { data } = action.payload,
+                newItem = { ...data, date: new Date() }
+
+            return [...state, newItem]
+
         default:
             return state
     }
+}
+
+// Action Creators
+export const Creators = {
+    onSaveRanking: data => ({
+        type: Types.SAVE_RANKING,
+        payload: { data },
+    }),
 }
