b6349dc46999ab0cafbadd3dc23e58df60f37a83
Marcelo Colla <colla.pr@gmail.com>
2019-07-21 16:08:12 -0300

    Ajustando tamanho do statusBar conforme notch do device

--- src/ui/layout.js
+++ src/ui/layout.js
@@ -1,5 +1,11 @@
+import DeviceInfo from 'react-native-device-info'
+
+const hasNotch = DeviceInfo.hasNotch()
+
 export default {
-    statusBar: 28,
+    hasNotch,
+    statusBar: hasNotch ? 28 : 16,
+    statusBarBottom: hasNotch ? 16 : 8,
     gutter: 12,
     gutterLarge: 16,
     radius: 8,
