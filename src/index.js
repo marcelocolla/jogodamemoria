d9aee643c269061133680acd52f0f8b263f8b599
Marcelo Colla <colla.pr@gmail.com>
2019-07-21 14:33:47 -0300

    Gravando ranking de usuário no storage

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
-
 import { Provider } from 'react-redux'
-import store from './store'
+import { PersistGate } from 'redux-persist/integration/react'
 
+import { store, persistor } from './store'
 import Routes from '~/routes'
 import NavigationService from '~/services/navigation'
 
 const App = () => (
     <Provider store={store}>
-        <Routes
-            ref={navRef => {
-                NavigationService.setNavigator(navRef)
-            }}
-        />
+        <PersistGate persistor={persistor}>
+            <Routes
+                ref={navRef => {
+                    NavigationService.setNavigator(navRef)
+                }}
+            />
+        </PersistGate>
     </Provider>
 )
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
 import createSagaMiddleware from 'redux-saga'
+import storage from 'redux-persist/lib/storage'
 
 import reducers from './ducks'
 import sagas from './sagas'
 
-const sagaMiddleware = createSagaMiddleware(),
-    store = createStore(reducers, applyMiddleware(sagaMiddleware))
+const config = {
+        key: 'root',
+        storage,
+        whitelist: ['ranking'],
+    },
+    persisted = persistReducer(config, reducers),
+    sagaMiddleware = createSagaMiddleware(),
+    store = createStore(persisted, applyMiddleware(sagaMiddleware)),
+    persistor = persistStore(store)
 
 sagaMiddleware.run(sagas)
 
-export default store
+export { store, persistor }
