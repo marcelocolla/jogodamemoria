c868dd520cfb8f96f2fa3beb39cfa16f0a47a308
Marcelo Colla <colla.pr@gmail.com>
2019-07-20 22:49:48 -0300

    Incluindo headerApp no pagina do Game

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-import { Title } from './styles'
 import { Router } from '~/components/basic'
+import HeaderApp from '~/components/headerApp'
 import Baralho from '~/components/baralho'
 import Toolbar from '~/components/toolbar'
 
 const PageGame = props => {
     return (
         <Router>
-            <Toolbar />
+            <HeaderApp />
 
             <Baralho />
 
