feaa04342615192e46e531c24385567e9a5929f9
Marcelo Colla <colla.pr@gmail.com>
2019-07-21 17:21:43 -0300

    Ajustando espaçamento do header para android sem notch

--- src/ui/layout.js
+++ src/ui/layout.js
@@ -1,10 +1,12 @@
+import { Platform } from 'react-native'
 import DeviceInfo from 'react-native-device-info'
 
-const hasNotch = DeviceInfo.hasNotch()
+const hasNotch = DeviceInfo.hasNotch(),
+    barHeight = Platform.OS === 'ios' ? 16 : 4
 
 export default {
     hasNotch,
-    statusBar: hasNotch ? 28 : 16,
+    statusBar: hasNotch ? 28 : barHeight,
     statusBarBottom: hasNotch ? 16 : 8,
     gutter: 12,
     gutterLarge: 16,
